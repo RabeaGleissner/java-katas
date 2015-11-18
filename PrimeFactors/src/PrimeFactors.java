9c34808b56d224daf6ed23b58d1bfbba33f222a4
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-18 13:45:11 +0000

    initial commit

---
+++ PrimeFactors/src/PrimeFactors.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rabeagleissner on 18/11/2015.
+ */
+public class PrimeFactors {
+}
---
+++ PrimeFactors/test/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import junit.framework.TestCase;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rabeagleissner on 18/11/2015.
+ */
+public class PrimeFactorsTest extends TestCase {
+}
