652cfa9654c84df9adbc5acfce5c6cf0ae06df7e
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-18 17:51:32 +0000

    tests for empty list when calculating  prime factors of 1

--- PrimeFactors/PrimeFactors-2/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-2/test/PrimeFactorsTest.java
@@ -1,7 +1,19 @@
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import junit.framework.TestCase;
+import primeFactors.PrimeFactors;
+
+import java.util.List;
 
 
 public class PrimeFactorsTest extends TestCase {
+    public void testOne() throws Exception {
+        assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    private List<Integer> list() {
+        return null;
+    }
 
 }
