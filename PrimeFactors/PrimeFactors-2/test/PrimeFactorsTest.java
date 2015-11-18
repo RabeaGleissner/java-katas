72d07d016a491345dbe0a0a6700266997899f75a
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-18 17:57:40 +0000

    refactors test

--- PrimeFactors/PrimeFactors-2/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-2/test/PrimeFactorsTest.java
@@ -9,12 +9,11 @@ import java.util.List;
 
 
 public class PrimeFactorsTest extends TestCase {
-    public void testOne() throws Exception {
-        assertEquals(list(), PrimeFactors.generate(1));
-    }
-
     private List<Integer> list() {
         return new ArrayList<Integer>();
     }
 
+    public void testOne() throws Exception {
+        assertEquals(list(), PrimeFactors.generate(1));
+    }
 }
