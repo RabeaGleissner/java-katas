19af525542da4f7f89c0cab556c1d1fa70310806
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 08:13:59 +0000

    tests for prime factors of 2 and returns list with varargs

--- PrimeFactors/PrimeFactors-2/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-2/test/PrimeFactorsTest.java
@@ -9,11 +9,21 @@ import java.util.List;
 
 
 public class PrimeFactorsTest extends TestCase {
+    private List<Integer> list(int... ints) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : ints)
+            list.add(i);
+        return list;
+    }
     private List<Integer> list() {
         return new ArrayList<Integer>();
     }
 
     public void testOne() throws Exception {
         assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    public void testTwo() throws Exception {
+        assertEquals(list(2),PrimeFactors.generate(2));
     }
 }
