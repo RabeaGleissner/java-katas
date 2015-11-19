b406f160a4c0724f48ab026bcd115932a6a61170
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 16:14:29 +0000

    replaces hard coded 2 with variable

--- PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
@@ -5,9 +5,10 @@ import java.util.*;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
-        while (n%2 == 0) {
-            primes.add(2);
-            n /= 2;
+        int candidate = 2;
+        while (n%candidate == 0) {
+            primes.add(candidate);
+            n /= candidate;
         }
         if (n > 1) {
             primes.add(n);
