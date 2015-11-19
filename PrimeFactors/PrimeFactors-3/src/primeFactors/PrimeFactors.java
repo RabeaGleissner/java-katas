62c05a121db60e92d054dd6d3613c5560dc4a434
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 16:18:31 +0000

    wrap block in if statement so it only runs when number is greater than 1

--- PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
@@ -5,14 +5,16 @@ import java.util.*;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
-        int candidate = 2;
-        while (n%candidate == 0) {
-            primes.add(candidate);
-            n /= candidate;
-        }
         if (n > 1) {
-            primes.add(n);
+            int candidate = 2;
+            while (n % candidate == 0) {
+                primes.add(candidate);
+                n /= candidate;
+            }
+            if (n > 1) {
+                primes.add(n);
 
+            }
         }
         return primes;
     }
