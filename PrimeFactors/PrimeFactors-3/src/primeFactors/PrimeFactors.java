4def65dd7c42ce68fbf5975ecc2dbebef4b02f04
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 16:05:31 +0000

    tests for prime factors of 8 and changes if to while loop

--- PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
-        if (n%2 == 0) {
+        while (n%2 == 0) {
             primes.add(2);
             n /= 2;
         }
