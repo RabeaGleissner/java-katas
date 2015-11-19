cdea869a2c8e049a4f8cbcd6e7ec6254c47bef70
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 14:30:28 +0000

    tests for prime factors of 6

--- PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactorsTest.java
@@ -27,4 +27,10 @@ public class PrimeFactorsTest {
         assertEquals(Arrays.asList(2,2),generate(4));
 
     }
+
+    @Test
+    public void testSix() {
+        assertEquals(Arrays.asList(2,3),generate(6));
+
+    }
 }
