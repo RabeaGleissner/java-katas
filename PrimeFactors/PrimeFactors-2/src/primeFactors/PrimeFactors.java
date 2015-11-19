0e8729d0f11effae64e5416a629b1b9c80ab25e0
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 12:24:00 +0000

    returns prime factors for 9

--- PrimeFactors/PrimeFactors-2/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-2/src/primeFactors/PrimeFactors.java
@@ -5,15 +5,16 @@ import java.util.*;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
-        if (n > 1) {
-            int candidate = 2;
+        int candidate = 2;
+        while (n > 1) {
             while (n%candidate == 0) {
                 primes.add(candidate);
                 n /= candidate;
             }
-            if (n > 1)
-                primes.add(n);
+            candidate++;
         }
+        if (n > 1)
+            primes.add(n);
         return primes;
     }
 }
