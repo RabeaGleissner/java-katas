a6543b5ce061f95e47e049e47bf6465c39c0f3cb
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 14:19:19 +0000

    importing generate method

--- PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactorsTest.java
@@ -3,17 +3,18 @@ package primeFactors;
 import org.junit.Test;
 import java.util.*;
 import static org.junit.Assert.*;
+import static primeFactors.PrimeFactors.generate;
 
 public class PrimeFactorsTest {
 
     @Test
     public void testOne() {
-       assertEquals(Arrays.asList(), PrimeFactors.generate(1));
+       assertEquals(Arrays.asList(), generate(1));
     }
 
     @Test
     public void testTwo() {
-        assertEquals(Arrays.asList(2), PrimeFactors.generate(2));
+        assertEquals(Arrays.asList(2), generate(2));
 
     }
 }
