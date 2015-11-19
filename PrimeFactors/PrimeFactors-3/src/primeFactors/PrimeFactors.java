fe53aa529cdd0ba81076f457f67d5d831bc1c510
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 14:28:18 +0000

    adds additional 2 to primes list of number modulus 2 is 0

--- PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-3/src/primeFactors/PrimeFactors.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
+        if (n%2 == 0) {
+            primes.add(2);
+            n /= 2;
+        }
         if (n > 1) {
             primes.add(n);
 
