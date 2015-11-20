f60c1f8893ef249ba9b5ea7bac984116333665e7
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 08:49:12 +0000

    remove duplication from test by statically importing generate method

--- PrimeFactors/PrimeFactors-4/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-4/test/PrimeFactorsTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 import primeFactors.PrimeFactors;
-
+import static primeFactors.PrimeFactors.generate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,12 +10,12 @@ import static org.junit.Assert.*;
 public class PrimeFactorsTest {
     @Test
     public void testOne() {
-        assertEquals(Arrays.asList(), PrimeFactors.generate(1));
+        assertEquals(Arrays.asList(),generate(1));
     }
 
     @Test
     public void testTwo() {
-        assertEquals(Arrays.asList(2),PrimeFactors.generate(2));
+        assertEquals(Arrays.asList(2),generate(2));
 
     }
 }
