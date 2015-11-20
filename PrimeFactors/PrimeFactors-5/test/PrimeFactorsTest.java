ae9edf603e618f1d0c68bc90c91a7ca2ed038ebe
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 14:06:33 +0000

    add private method for list that returns null

--- PrimeFactors/PrimeFactors-5/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-5/test/PrimeFactorsTest.java
@@ -1,12 +1,19 @@
 import org.junit.Test;
 import primeFactors.PrimeFactors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PrimeFactorsTest {
     @Test
     public void testOne() {
         assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    private List<Integer> list() {
+        return null;
     }
 
 }
