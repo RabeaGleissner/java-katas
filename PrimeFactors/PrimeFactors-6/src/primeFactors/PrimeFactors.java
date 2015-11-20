21489f09a8c756243ed74a27ca1290d670533127
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 17:05:32 +0000

    save array list in variable, add 2 to array list

--- PrimeFactors/PrimeFactors-6/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-6/src/primeFactors/PrimeFactors.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class PrimeFactors {
 
-    public static List<Integer> generate(int i) {
-        return new ArrayList<Integer>();
+    public static List<Integer> generate(int n) {
+        List<Integer> primes = new ArrayList<Integer>();
+        if (n > 1) {
+           primes.add(2);
+        }
+        return primes;
     }
 }
