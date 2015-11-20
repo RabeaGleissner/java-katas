fd440ce6ccf2363a7e8803913dc9e8dc1207166e
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 14:55:35 +0000

    remove curly braces from for loops

--- PrimeFactors/PrimeFactors-5/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-5/src/primeFactors/PrimeFactors.java
@@ -6,11 +6,9 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
-        for (int candidate = 2; n > 1; candidate++) {
-            for (; n % candidate == 0; n/= candidate) {
+        for (int candidate = 2; n > 1; candidate++)
+            for (; n % candidate == 0; n/= candidate)
                 primes.add(candidate);
-            }
-        }
         return primes;
     }
 }
