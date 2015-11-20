88c73bde2a00753035e9705b8fff19c56034c840
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 14:37:16 +0000

    adds 2 to array list if number modulus 2 is 0

--- PrimeFactors/PrimeFactors-5/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-5/src/primeFactors/PrimeFactors.java
@@ -8,8 +8,12 @@ public class PrimeFactors {
         List<Integer> primes = new ArrayList<Integer>();
 
         if (n > 1) {
-            primes.add(n);
-
+            if (n%2 == 0) {
+                primes.add(2);
+                n /= 2;
+            }
+            if (n > 1)
+                primes.add(n);
         }
         return primes;
     }
