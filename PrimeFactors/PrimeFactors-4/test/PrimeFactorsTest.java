5d0a9f305a626325b3031e3e0db1f86e88aae9e4
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 08:41:55 +0000

    test for prime factors of 2 and use Arrays as list

--- PrimeFactors/PrimeFactors-4/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-4/test/PrimeFactorsTest.java
@@ -2,18 +2,20 @@ import org.junit.Test;
 import primeFactors.PrimeFactors;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class PrimeFactorsTest {
-    private List<Integer> list () {
-        return new ArrayList<Integer>();
+    @Test
+    public void testOne() {
+        assertEquals(Arrays.asList(), PrimeFactors.generate(1));
     }
 
     @Test
-    public void testOne() {
-        assertEquals(list(), PrimeFactors.generate(1));
-    }
+    public void testTwo() {
+        assertEquals(Arrays.asList(2),PrimeFactors.generate(2));
 
+    }
 }
