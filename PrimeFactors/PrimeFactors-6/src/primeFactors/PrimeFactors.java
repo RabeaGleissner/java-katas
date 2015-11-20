d30f5c59c33871ed9ad87e89314525e527521c2c
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 17:20:41 +0000

    place variable assignement and second if statement outside the main if statement

--- PrimeFactors/PrimeFactors-6/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-6/src/primeFactors/PrimeFactors.java
@@ -7,15 +7,15 @@ public class PrimeFactors {
 
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
+        int candidate = 2;
         if (n > 1) {
-            int candidate = 2;
             while (n%candidate == 0) {
                 primes.add(candidate);
                 n/=candidate;
             }
-            if (n > 1)
-                primes.add(n);
         }
+        if (n > 1)
+            primes.add(n);
         return primes;
     }
 }
