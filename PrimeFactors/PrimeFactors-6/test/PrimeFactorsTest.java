7879f0d7819a630a43c1e0ef5fe3228ff6bc4e60
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 16:52:04 +0000

    test for prime factors of 1

--- PrimeFactors/PrimeFactors-6/test/PrimeFactorsTest.java
+++ PrimeFactors/PrimeFactors-6/test/PrimeFactorsTest.java
@@ -1,5 +1,17 @@
+import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PrimeFactorsTest {
 
+    private List<Integer> list() {
+        return null;
+    }
+    @Test
+    public void testOne() {
+        assertEquals(list(),PrimeFactors.generate(1));
+
+    }
 }
