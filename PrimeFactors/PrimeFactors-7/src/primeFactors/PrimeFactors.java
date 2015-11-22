2084bdd59e50a4c44b448450f87622f96e2037a2
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 08:48:57 +0000

    add generate method which returns an empty array list

--- PrimeFactors/PrimeFactors-7/src/primeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors-7/src/primeFactors/PrimeFactors.java
@@ -1,4 +1,11 @@
 package primeFactors;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
+    public static List<Integer> generate(int n) {
+        return new ArrayList<Integer>();
+    }
 }
