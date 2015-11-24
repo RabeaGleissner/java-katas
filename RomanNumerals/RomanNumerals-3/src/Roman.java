28ea0efa5dbd2a6f480abe693e81a76b60a3d088
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 15:42:09 +0000

    refactored to use for loop and arrays with constants

--- RomanNumerals/RomanNumerals-3/src/Roman.java
+++ RomanNumerals/RomanNumerals-3/src/Roman.java
@@ -2,21 +2,16 @@ public class Roman {
     public static String convert(int arabic) {
         String roman = "";
 
+        int[] ARABIC = {10, 5, 1};
+        String[] ROMAN = {"X", "V", "I"};
 
-
-        while (arabic >= 10) {
-            roman += "X";
-            arabic -=10;
-        }
-        while (arabic >= 5) {
-            roman += "V";
-            arabic -= 5;
+        for (int i = 0; i < ARABIC.length; i++) {
+            while (arabic >= ARABIC[i]) {
+                roman += ROMAN[i];
+                arabic -= ARABIC[i];
+            }
         }
 
-        while(arabic >= 1) {
-            roman += "I";
-            arabic -= 1;
-        }
         return roman;
     }
 }
