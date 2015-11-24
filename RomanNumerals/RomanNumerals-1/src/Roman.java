1339d59ffb091fdd67ae7dd79677d00433b2789e
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 12:21:11 +0000

    changed folder structure

---
+++ RomanNumerals/RomanNumerals-1/src/Roman.java
@@ -0,0 +1,15 @@
+public class Roman {
+    public static String convert(int arabic) {
+        int[] ARABIC = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] ROMAN = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        String roman = "";
+        for (int i = 0; i < ARABIC.length; i++) {
+            while (arabic >= ARABIC[i]) {
+                roman += ROMAN[i];
+                arabic -= ARABIC[i];
+            }
+        }
+        return roman;
+    }
+}
---
+++ RomanNumerals/RomanNumerals-1/src/RomanTest.java
@@ -0,0 +1,67 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RomanTest {
+    @Test
+    public void convertOne() {
+        assertEquals("I", Roman.convert(1));
+    }
+
+    @Test
+    public void convertTwo() {
+        assertEquals("II", Roman.convert(2));
+    }
+    @Test
+    public void convertThree() {
+        assertEquals("III", Roman.convert(3));
+    }
+
+    @Test
+    public void converFive() {
+        assertEquals("V", Roman.convert(5));
+    }
+
+    @Test
+    public void convertTen() {
+        assertEquals("X", Roman.convert(10));
+    }
+
+    @Test
+    public void convertTwenty() {
+        assertEquals("XX", Roman.convert(20));
+    }
+
+    @Test
+    public void convertThirty() {
+        assertEquals("XXX", Roman.convert(30));
+
+    }
+
+    @Test
+    public void convertEleven() {
+        assertEquals("XI", Roman.convert(11));
+    }
+
+    @Test
+    public void convertEight() {
+        assertEquals("VIII", Roman.convert(8));
+    }
+
+    @Test
+    public void convertFour() {
+        assertEquals("IV", Roman.convert(4));
+
+    }
+
+    @Test
+    public void convertCurrentYear() {
+        assertEquals("MMXV", Roman.convert(2015));
+
+    }
+
+    @Test
+    public void convert3999() {
+        assertEquals("MMMCMXCIX", Roman.convert(3999));
+    }
+}
