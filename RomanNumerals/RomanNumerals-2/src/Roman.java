e680b2724667d8a1e544571c7350b7f73e9cef5a
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 15:09:23 +0000

    WIP attempt to use a HashMap

--- RomanNumerals/RomanNumerals-2/src/Roman.java
+++ RomanNumerals/RomanNumerals-2/src/Roman.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Roman {
 
@@ -9,14 +10,20 @@ public class Roman {
         arabicToRoman.put(10, "X");
 
         String roman = "";
-        if (arabic >= 10) {
-            roman += arabicToRoman.get(10);
-            arabic -= 10;
+        for (Map.Entry<Integer, String> entry : arabicToRoman.entrySet()) {
+            if (arabic >= arabicToRoman.getKey()){
+                roman += arabicToRoman.getValue();
+                arabic -= arabicToRoman.getKey();
+            }
         }
-        if (arabic >= 5) {
-            roman += arabicToRoman.get(5);
-            arabic -= 5;
-        }
+//        if (arabic >= 10) {
+//            roman += arabicToRoman.get(10);
+//            arabic -= 10;
+//        }
+//        if (arabic >= 5) {
+//            roman += arabicToRoman.get(5);
+//            arabic -= 5;
+//        }
         for (int i=0; i < arabic; i++) {
             roman += arabicToRoman.get(1);
         }
