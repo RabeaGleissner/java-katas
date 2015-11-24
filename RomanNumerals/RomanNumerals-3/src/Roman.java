a1d8f93bf9672c2490695bb229d2a9dfc1720124
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 15:39:06 +0000

    changed all three while loops so that they're doing the same operations

--- RomanNumerals/RomanNumerals-3/src/Roman.java
+++ RomanNumerals/RomanNumerals-3/src/Roman.java
@@ -2,18 +2,20 @@ public class Roman {
     public static String convert(int arabic) {
         String roman = "";
 
+
+
         while (arabic >= 10) {
             roman += "X";
             arabic -=10;
         }
-        if (arabic >= 5) {
+        while (arabic >= 5) {
             roman += "V";
             arabic -= 5;
         }
 
-        while(arabic > 0) {
+        while(arabic >= 1) {
             roman += "I";
-            arabic --;
+            arabic -= 1;
         }
         return roman;
     }
