006d15994379483422c7ebb558445f259e690726
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 14:44:02 +0000

    using hashmap to return roman numeral values

--- RomanNumerals/RomanNumerals-2/src/Roman.java
+++ RomanNumerals/RomanNumerals-2/src/Roman.java
@@ -1,18 +1,28 @@
+import java.util.HashMap;
+
 public class Roman {
 
     public static String convert(int arabic) {
+        HashMap<Integer, String> arabicToRoman = new HashMap<Integer, String>();
+        arabicToRoman.put(1, "I");
+        arabicToRoman.put(5, "V");
+        arabicToRoman.put(10, "X");
+
         String roman = "";
         if (arabic >= 10) {
-            roman += "X";
+            roman += arabicToRoman.get(10);
             arabic -= 10;
         }
         if (arabic >= 5) {
-            roman += "V";
+            roman += arabicToRoman.get(5);
             arabic -= 5;
         }
         for (int i=0; i < arabic; i++) {
-            roman += "I";
+            roman += arabicToRoman.get(1);
         }
         return roman;
     }
+
+
+
 }
