400733daba1c1e5bf7c593caaacbab461af1df39
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 11:18:20 +0000

    add 9th attempt of the PrimeFactors kata

---
+++ PrimeFactors/PrimeFactors-9/src/primeFactors/PrimeFactorsTest.java
@@ -0,0 +1,55 @@
+package primeFactors;
+import static primeFactors.PrimeFactors.generate;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PrimeFactorsTest {
+    private List<Integer> list(int... ints) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : ints) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(list(), generate(1));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(list(2), generate(2));
+    }
+
+    @Test
+    public void testThree() {
+        assertEquals(list(3), generate(3));
+
+    }
+
+    @Test
+    public void testFour() {
+        assertEquals(list(2,2), generate(4));
+    }
+
+    @Test
+    public void testSix() {
+        assertEquals(list(2,3), generate(6));
+
+    }
+
+    @Test
+    public void testEight() {
+        assertEquals(list(2,2,2), generate(8));
+    }
+
+    @Test
+    public void testNine() {
+        assertEquals(list(3,3), generate(9));
+    }
+}
