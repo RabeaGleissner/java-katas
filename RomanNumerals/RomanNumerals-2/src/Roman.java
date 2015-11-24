c25b7683f4fc0bc722fcc60729a0bf50dbf9449b
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-24 13:33:42 +0000

    converts 2 into Roman number

--- RomanNumerals/RomanNumerals-2/src/Roman.java
+++ RomanNumerals/RomanNumerals-2/src/Roman.java
@@ -1,6 +1,9 @@
 public class Roman {
 
     public static String convert(int arabic) {
+        if (arabic > 1) {
+            return "II";
+        }
         return "I";
     }
 }
--- RomanNumerals/RomanNumerals-2/src/RomanTest.java
+++ RomanNumerals/RomanNumerals-2/src/RomanTest.java
@@ -7,4 +7,9 @@ public class RomanTest {
     public void convert1() {
         assertEquals("I", Roman.convert(1));
     }
+
+    @Test
+    public void convert2() {
+        assertEquals("II", Roman.convert(2));
+    }
 }
