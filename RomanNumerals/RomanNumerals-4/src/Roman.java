9cfb19cade6edf322ecb4b8a6ed34bdb9166c96f
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-25 13:12:00 +0000

    refactored using for loop and look up aarrays

--- RomanNumerals/RomanNumerals-4/src/Roman.java
+++ RomanNumerals/RomanNumerals-4/src/Roman.java
@@ -1,17 +1,14 @@
 public class Roman {
     public static String convert(int arabic) {
+        int[] ARABIC = {10, 5, 1};
+        String[] ROMAN = {"X", "V", "I"};
+
         String roman = "";
-        while (arabic >= 10) {
-            roman += "X";
-            arabic -= 10;
-        }
-        if (arabic >= 5) {
-            roman += "V";
-            arabic -= 5;
-        }
-        while (arabic >= 1) {
-            roman += "I";
-            arabic--;
+        for (int i = 0; i < ARABIC.length; i++) {
+            while (arabic >= ARABIC[i]) {
+                roman += ROMAN[i];
+                arabic -= ARABIC[i];
+            }
         }
         return roman;
     }
