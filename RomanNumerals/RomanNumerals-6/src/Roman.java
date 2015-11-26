654a190cbbf544e862285106e0265f079d343ce9
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-26 08:28:21 +0000

    converts 2 to II

--- RomanNumerals/RomanNumerals-6/src/Roman.java
+++ RomanNumerals/RomanNumerals-6/src/Roman.java
@@ -1,5 +1,8 @@
 public class Roman {
     public static String convert(int arabic) {
+        if (arabic == 2) {
+            return "II";
+        }
         return "I";
     }
 }
--- RomanNumerals/RomanNumerals-6/src/RomanTest.java
+++ RomanNumerals/RomanNumerals-6/src/RomanTest.java
@@ -8,4 +8,10 @@ public class RomanTest {
     public void convert1() {
         assertEquals("I", Roman.convert(1));
     }
+
+    @Test
+    public void convert2() {
+        assertEquals("II", Roman.convert(2));
+
+    }
 }
