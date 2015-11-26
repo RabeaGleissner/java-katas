072ee5881521e3c79292f5e92e6160ab5ee3ecd6
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-26 08:41:26 +0000

    converts 100

--- RomanNumerals/RomanNumerals-6/src/Roman.java
+++ RomanNumerals/RomanNumerals-6/src/Roman.java
@@ -2,6 +2,11 @@ public class Roman {
     public static String convert(int arabic) {
         String roman = "";
 
+        while (arabic >= 100) {
+            roman += "C";
+            arabic -= 100;
+        }
+
         while (arabic >= 10) {
             roman += "X";
             arabic -= 10;
