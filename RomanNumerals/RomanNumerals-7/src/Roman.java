3933b0a5447d3265ddb6a7d4d829171bdd3041ec
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-26 10:56:31 +0000

    converts 4

--- RomanNumerals/RomanNumerals-7/src/Roman.java
+++ RomanNumerals/RomanNumerals-7/src/Roman.java
@@ -2,8 +2,8 @@ public class Roman {
     public static String convert(int arabic) {
         String roman = "";
 
-        int[] ARABIC = {100, 10, 1};
-        String[] ROMAN = {"C", "X", "I"};
+        int[] ARABIC = {100, 10, 4, 1};
+        String[] ROMAN = {"C", "X", "IV", "I"};
 
         for (int i = 0; i < ARABIC.length; i++ ) {
             while (arabic >= ARABIC[i]) {
