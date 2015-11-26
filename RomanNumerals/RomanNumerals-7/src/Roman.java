55f80094179ea0c7a86a44618daeb4a492e13cca
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-26 10:50:05 +0000

    converts 10 to X

--- RomanNumerals/RomanNumerals-7/src/Roman.java
+++ RomanNumerals/RomanNumerals-7/src/Roman.java
@@ -1,6 +1,10 @@
 public class Roman {
     public static String convert(int arabic) {
         String roman = "";
+        if (arabic >= 10) {
+            roman += "X";
+            arabic -= 10;
+        }
 
         while (arabic >= 1) {
             roman += "I";
