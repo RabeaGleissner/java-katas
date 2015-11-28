d28619579661bd9d2ffe93e8ed2180f261b80118
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-28 13:44:46 +0000

    converts 1 into I

--- RomanNumerals/RomanNumerals-10/src/RomanNumerals.java
+++ RomanNumerals/RomanNumerals-10/src/RomanNumerals.java
@@ -1,2 +1,5 @@
 public class RomanNumerals {
+    public static String convert(int arabic) {
+        return "I";
+    }
 }
--- RomanNumerals/RomanNumerals-10/src/RomanNumeralsTest.java
+++ RomanNumerals/RomanNumerals-10/src/RomanNumeralsTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RomanNumeralsTest {
 
+    @Test
+    public void convert1() {
+        assertEquals("I", RomanNumerals.convert(1));
+    }
 }
