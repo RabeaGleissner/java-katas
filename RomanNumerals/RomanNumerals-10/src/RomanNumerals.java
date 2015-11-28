cf0b828f8642ebf3531cf9dde4432b6fa90fdc09
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-28 14:54:03 +0000

    converts 3 to III

--- RomanNumerals/RomanNumerals-10/src/RomanNumerals.java
+++ RomanNumerals/RomanNumerals-10/src/RomanNumerals.java
@@ -1,6 +1,10 @@
 public class RomanNumerals {
     public static String convert(int arabic) {
 
+        if (arabic == 3) {
+            return "III";
+        }
+
         if (arabic == 2) {
             return "II";
         }
