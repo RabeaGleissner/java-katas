0c4b0a427ed36400bc4d50d5b1449b5def72cfe6
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-30 20:14:56 +0000

    use for loop instead of several while/if statements

--- CoinChanger/CoinChanger-1/src/CoinChanger.java
+++ CoinChanger/CoinChanger-1/src/CoinChanger.java
@@ -5,19 +5,14 @@ public class CoinChanger {
     public static List<Integer> giveChange(int amount) {
 
         List<Integer> change = new ArrayList<>();
+        int[] AVAILABLE_COINS = {10, 5, 1};
 
-        if (amount >= 10) {
-            change.add(10);
-            amount -= 10;
-        }
+        for (int i =0; i < AVAILABLE_COINS.length; i++) {
+            while (amount >= AVAILABLE_COINS[i]) {
+                change.add(AVAILABLE_COINS[i]);
+                amount -= AVAILABLE_COINS[i];
+            }
 
-        if (amount >= 5) {
-            change.add(5);
-            amount -= 5;
-        }
-        while (amount >= 1) {
-            change.add(1);
-            amount --;
         }
         return change;
     }
