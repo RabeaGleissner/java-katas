7002a3973cfce5e05fd39b6adc3924cf83d67678
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-30 19:58:01 +0000

    use while loop instead of seperate if statements

--- CoinChanger/CoinChanger-1/src/CoinChanger.java
+++ CoinChanger/CoinChanger-1/src/CoinChanger.java
@@ -5,16 +5,11 @@ public class CoinChanger {
     public static List<Integer> giveChange(int amount) {
 
         List<Integer> change = new ArrayList<>();
-        if (amount >= 3) {
+
+        while (amount >= 1) {
             change.add(1);
+            amount --;
         }
-
-        if (amount >= 2) {
-            change.add(1);
-        }
-
-        change.add(1);
-
         return change;
     }
 }
