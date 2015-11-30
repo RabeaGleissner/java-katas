4e05756c2e8e974eb883de08521a1caca5d4bbbd
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-30 19:55:05 +0000

    gives change for 2

--- CoinChanger/CoinChanger-1/src/CoinChangerTest.java
+++ CoinChanger/CoinChanger-1/src/CoinChangerTest.java
@@ -1,11 +1,24 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class CoinChangerTest {
 
+    private ArrayList<Integer> coins(Integer...i) {
+        return new ArrayList<Integer>(Arrays.asList(i));
+    }
+
     @Test
     public void changeFor1() {
-        assertEquals(1, CoinChanger.giveChange(1));
+        assertEquals(coins(1), CoinChanger.giveChange(1));
     }
+
+    @Test
+    public void changeFor2() {
+        assertEquals(coins(1,1), CoinChanger.giveChange(2));
+    }
+
 }
