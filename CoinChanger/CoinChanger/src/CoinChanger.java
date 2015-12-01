cc5255f9be1973586341ead94914b73a6e8a3b57
Rabea Gleissner <rabeagleissner@gmail.com>
2015-12-01 10:47:37 +0000

    gives change for 2

--- CoinChanger/CoinChanger/src/CoinChanger.java
+++ CoinChanger/CoinChanger/src/CoinChanger.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoinChanger {
-    public static int giveChane(int amount) {
-        return 1;
+
+    public static List<Integer> giveChange(int amount) {
+
+        List<Integer> change = new ArrayList<>();
+
+        if (amount >= 2) {
+            change.add(1);
+        }
+        change.add(1);
+
+        return change;
     }
 }
--- CoinChanger/CoinChanger/src/CoinChangerTest.java
+++ CoinChanger/CoinChanger/src/CoinChangerTest.java
@@ -1,11 +1,25 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CoinChangerTest {
 
+    private ArrayList<Integer> coins(Integer...i) {
+        return new ArrayList<>(Arrays.asList(i));
+    }
+
     @Test
     public void changeFor1() {
-        assertEquals(1, CoinChanger.giveChane(1));
+        assertEquals(coins(1), CoinChanger.giveChange(1));
+    }
+
+    @Test
+    public void changeFor2() {
+        assertEquals(coins(1,1), CoinChanger.giveChange(2));
+
     }
 }
