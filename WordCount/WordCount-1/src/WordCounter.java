70e10675366fbe38f061d87a6d4dce267e0cef76
Rabea Gleissner <rabeagleissner@gmail.com>
2015-12-07 14:42:27 +0000

    count multiple occurences of the same word

--- WordCount/WordCount-1/src/WordCounter.java
+++ WordCount/WordCount-1/src/WordCounter.java
@@ -1,13 +1,19 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
     public Map<String, Integer> phrase(String phrase) {
+
         Map<String, Integer> wordsWithCount = new HashMap<>();
+
         String[] words = phrase.split(" ");
+
         for (String word : words) {
-            wordsWithCount.put(word, 1);
+            Integer count = 0;
+            if (wordsWithCount.containsKey(word)) {
+                count = wordsWithCount.get(word);
+            }
+            wordsWithCount.put(word, ++count);
         }
         return wordsWithCount;
     }
